624ea5517cbea92811de9e7615c832362fab3c82
GTrunSec <gtrunsec@hardenedlinux.org>
2021-06-27 17:35:03 -0700

    WIP: setup btest

--- flake.nix
+++ flake.nix
@@ -6,14 +6,25 @@
     nixpkgs.url = "nixpkgs/release-21.05";
     flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
     devshell-flake.url = "github:numtide/devshell";
-    zeek.url = "github:hardenedlinux/zeek-nix";
+    zeek2nix.url = "github:hardenedlinux/zeek-nix";
+    spicy-with-nix-flake.url = "github:GTrunSec/spicy-with-nix-flake";
     nixpkgs-hardenedlinux.url = "github:hardenedlinux/nixpkgs-hardenedlinux";
     nvfetcher = {
       url = "github:berberman/nvfetcher";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, flake-utils, flake-compat, devshell-flake, nvfetcher, zeek, nixpkgs-hardenedlinux }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , flake-compat
+    , devshell-flake
+    , nvfetcher
+    , zeek2nix
+    , spicy-with-nix-flake
+    , nixpkgs-hardenedlinux
+    }:
     {
       overlay = final: prev:
         {
@@ -24,50 +35,52 @@
     //
     (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
       (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlay
-              devshell-flake.overlay
-              zeek.overlay
-              nixpkgs-hardenedlinux.overlay
-              nvfetcher.overlay
-            ];
-            config = {
-              allowUnsupportedSystem = true;
-            };
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlay
+            devshell-flake.overlay
+            zeek2nix.overlay
+            nixpkgs-hardenedlinux.overlay
+            nvfetcher.overlay
+            spicy-with-nix-flake.overlay
+          ];
+          config = {
+            allowUnsupportedSystem = true;
           };
-        in
-        rec {
-          packages = flake-utils.lib.flattenTree rec {
-            zeek-release = pkgs.zeek-release;
-            hardenedlinux-zeek-scripts = pkgs.hardenedlinux-zeek-scripts;
-          };
+        };
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree rec {
+          zeek-release = pkgs.zeek-release;
+          hardenedlinux-zeek-scripts = pkgs.hardenedlinux-zeek-scripts;
+        };
 
-          hydraJobs = {
-            inherit packages;
-          };
+        hydraJobs = {
+          inherit packages;
+        };
 
-          devShell = with pkgs; devshell.mkShell {
-            imports = [
-              (devshell.importTOML ./nix/devshell.toml)
-            ];
-            packages = [
-              zeek-release
-              (pkgs.python3.withPackages (ps: with ps;[
-                btest
-              ]))
-            ];
-            commands = [
-              {
-                name = pkgs.nvfetcher-bin.pname;
-                help = pkgs.nvfetcher-bin.meta.description;
-                command = "cd $DEVSHELL_ROOT/scripts; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml --no-output $@; nixpkgs-fmt _sources";
-              }
-            ];
-          };
-        }
+        devShell = with pkgs; devshell.mkShell {
+          imports = [
+            (devshell.importTOML ./nix/devshell.toml)
+          ];
+          packages = [
+            zeek-release
+            (pkgs.python3.withPackages (ps: with ps;[
+              btest
+            ]))
+            zed
+          ];
+          commands = [
+            {
+              name = pkgs.nvfetcher-bin.pname;
+              help = pkgs.nvfetcher-bin.meta.description;
+              command = "cd $DEVSHELL_ROOT/scripts; ${pkgs.nvfetcher-bin}/bin/nvfetcher -c ./sources.toml --no-output $@; nixpkgs-fmt _sources";
+            }
+          ];
+        };
+      }
       )
     );
 }
